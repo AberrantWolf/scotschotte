62c15f8c6c7152a77472d420ec2365c1d9ee806f
AberrantWolf <orcein@gmail.com>
2019-08-17 23:14:25 +0900

    (in-progress) Resetting app with Vulkan
    Refactoring Vulkano example to have some structs so it can be useful for more than just a single triangle.

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,14 @@
-extern crate gdk_pixbuf;
-extern crate gio;
-extern crate gtk;
+// mod components;
+// #[macro_use]
+// mod ui_util;
 
-mod components;
-#[macro_use]
-mod ui_util;
+mod sc_core;
 
-#[allow(unused_imports)]
-use gio::prelude::*;
-use gtk::prelude::*;
-#[allow(unused_imports)]
-use gtk::{
-    AboutDialog, AccelFlags, AccelGroup, ApplicationWindow, CheckMenuItem, EventBox,
-    FileChooserAction, FileChooserDialog, IconSize, Image, Label, Layout, Menu, MenuBar, MenuItem,
-    ResponseType, ScrolledWindowBuilder, WidgetExt, Window, WindowPosition,
-};
-
-use components::schotte_core::SchotteApp;
+// use components::schotte_core::SchotteApp;
 use std::env::args;
 
 fn main() {
-    println!("I. LiVe. AgAiN.");
-
-    let application = gtk::Application::new(
-        Some("com.github.gtk-rs.examples.menu_bar"),
-        Default::default(),
-    )
-    .expect("Initialization failed...");
-
-    application.connect_activate(|app| {
-        SchotteApp::build(app);
-    });
-    application.run(&args().collect::<Vec<_>>());
+    println!("I. Am. Vulkanized.");
+    let app = sc_core::init_app().unwrap();
+    app.run();
 }
